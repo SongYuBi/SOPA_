a8f79b89ad9ebc4611d77b6cdd9cc0d195b6b731
Song <Song@DESKTOP-AH7Q24J>
2020-06-20 20:03:32 +0900

    서버구현 하였고 이제 UI와 로그인룰렀을시 연결되는거

--- src/com/kh/sopa/run/Client_run.java
+++ src/com/kh/sopa/run/Client_run.java
@@ -1,12 +1,12 @@
-package com.kh.sopa.controller;
+package com.kh.sopa.run;
 
 import com.kh.sopa.view.Main_Frame;
 
-public class HomeController {
+public class Client_run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		new Main_Frame();
 	}
 
 }
--- src/com/kh/sopa/view/Stand_Room.java
+++ src/com/kh/sopa/view/Stand_Room.java
@@ -3,20 +3,34 @@ package com.kh.sopa.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Desktop.Action;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
-public class Stand_Room extends JFrame{
+import com.kh.sopa.controller.Client_Contorller;
+
+public class Stand_Room extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
 	private JTextArea jta = new JTextArea(25, 25);
 	private JTextField jtf = new JTextField(25);
-
-	public Stand_Room() {
+	private Client_Contorller client = new  Client_Contorller();
+	private String user_id;
+	private JLabel user_label = new JLabel(); 
+	//panel_stand_room -> 대기실 기본으로된 패널
+	//panel_south_menu -> 하단에 메뉴바 
+	//panel_south_menu_chat -> 하단에 메뉴바 안에 채팅만 들어가게해놓은 곳
+	//panel_north_userhi(라벨) -> 유저가 들어오면 들어온 유저를 알리는 곳.
+	public Stand_Room(String user_id) {
+		this.user_id = user_id;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1024, 768);
 		contentPane = new JPanel();
@@ -31,6 +45,12 @@ public class Stand_Room extends JFrame{
 		panel_stand_room.setBounds(0, 0, 1024, 768);
 		panel_stand_room.setBackground(Color.white);
 
+		user_label.setFont(new Font("맑은 고딕", Font.PLAIN, 30));
+		user_label.setBackground(Color.YELLOW);
+		user_label.setBounds(0, 0, 1024, 100);
+		
+		panel_stand_room.add(user_label,BorderLayout.NORTH);
+		
 		JPanel panel_south_menu = new JPanel();			//밑에 메뉴 패널
 		
 		panel_south_menu.setBackground(Color.gray);
@@ -43,6 +63,8 @@ public class Stand_Room extends JFrame{
 		jta.setEditable(false);
 		jta.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
 		jta.setBackground(new Color(230, 255, 230));
+		jtf.addActionListener(this);
+		
 		panel_south_menu.add(panel_south_menu_chat);
 		panel_south_menu_chat.add(jta, BorderLayout.CENTER);
 		panel_south_menu_chat.add(jtf, BorderLayout.SOUTH);
@@ -54,6 +76,31 @@ public class Stand_Room extends JFrame{
 		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		client.setGui(this);
+		client.setNicknames(user_id);			
+		client.connect(user_id);
+	}
+	
+	// 채팅필드 이벤트
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//문자 입력 창에 글을 입력 시 구동할 이벤트 설정
+		//스트링 변수 msdg에 닉네임 : 텍스트필드의 내용과 밑줄로 내려가기를 담아
+		String msg = user_id + ": " + jtf.getText() +"\n";
+		//메소드에 내용을 보낸다
+		client.sendMessage(msg);
+		//텍스트에리어 비우기
+		jtf.setText("");
 	}
 
+	//채팅창에 유저가 보낸 메시지 추가
+	public void appendMsg(String msg) {
+		jta.append(msg);
+		
+	}
+	//유저가 접속하면 유저아이디로 인사.
+	public void label_userid(String user_id) {
+		user_label.setText(user_id+"님 환영합니다.!!!");
+	}
 }
